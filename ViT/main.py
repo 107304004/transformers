cf7fab4c6cbbf1fb37ec0d93ecd4d9305e8dc98d
107304004 <win2462424@gmail.com>
2023-07-17 15:05:00 +0800

    init

---
+++ ViT/main.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+from torchvision.models import vit_b_16
+from tqdm import tqdm
+
+
+# Set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Hyperparameters
+num_epochs = 10
+batch_size = 128
+learning_rate = 0.0001
+
+# Data preprocessing
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+# CIFAR-10 dataset (train:5000o, test:10000)
+train_dataset = CIFAR10(root="../data", train=True, download=True, transform=transform)
+test_dataset = CIFAR10(root="../data", train=False, download=True, transform=transform)
+
+# Data loaders
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+# Model
+model = vit_b_16()
+# print(model)
+model.heads.head = nn.Linear(model.heads.head.in_features, 10)
+# print(model)
+model.to(device)
+
+# Loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# Training loop
+total_step = len(train_loader)
+for epoch in range(num_epochs):
+    total_loss = 0
+    for i, (images, labels) in enumerate(tqdm(train_loader)):
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        total_loss += loss.item()
+
+        # Backward pass and optimization
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    # Print training progress
+    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {total_loss}")
+
+# Evaluation
+model.eval()
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in tqdm(test_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+
+        # Update accuracy
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    accuracy = correct / total
+    print(f"Test Accuracy: {accuracy * 100}%")
