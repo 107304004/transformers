1aa7f44f6e19ea00d7206e90ce25a659ae9456b6
107304004 <win2462424@gmail.com>
2023-07-31 21:55:41 +0800

    add pretrained vit_b_16

--- ViT/main.py
+++ ViT/main.py
@@ -15,7 +15,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Hyperparameters
 num_epochs = 5
 batch_size = 128
-learning_rate = 0.0001
+learning_rate = 0.0002
 
 # Data preprocessing
 transform = transforms.Compose([
@@ -31,22 +31,29 @@ test_dataset = CIFAR10(root="../data", train=False, download=True, transform=tra
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-# Model
-# handcraft ViT
-print("Handcraft ViT:")
-model = ViT(image_size=224, patch_size=16, num_classes=10, dim=768, depth=12, heads=12, mlp_dim=3072)
-model.to(device)
+# Models
+# 1. handcraft ViT
+# print("Handcraft ViT:")
+# model = ViT(image_size=224, patch_size=16, num_classes=10, dim=768, depth=12, heads=12, mlp_dim=3072)
+# model.to(device)
 
-
-# Torch ViT
+# 2. Torch ViT
 # print("Torch ViT")
 # model = vit_b_16()
 # # print(model)
 # model.heads.head = nn.Linear(model.heads.head.in_features, 10)
 # # print(model)
 # model.to(device)
-# model.load_state_dict(torch.load('./checkpoint/ViT.ckpt'))
 
+# 3. Torch ViT
+print("Torch Pretrained ViT")
+model = vit_b_16(pretrained=True)
+model.heads.head = nn.Linear(model.heads.head.in_features, 10)
+model.to(device)
+
+
+# load model checkpoint
+# model.load_state_dict(torch.load('./checkpoint/ViT.ckpt'))
 # Loss function and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
